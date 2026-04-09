65953e153ad61c6237f37e8c2f3e63717b1921f2
syabilpcr <syabil24si@mahasiswa.pcr.ac.id>
2026-04-09 16:09:26 +0700

    P4 ResponsiceText

--- src/pertemuan-4/main.jsx
+++ src/pertemuan-4/main.jsx
@@ -2,11 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './tailwind.css'
 import FrameworkList from './FrameworkList'
 import FrameworkListSearchFilter from './FrameworkListSeacrhFilter'
+import ResponsiveText from './ResponsiveText'
 
 createRoot(document.getElementById("root"))
     .render(
       <div classname="card">
         {/* <FrameworkList/> */}
-        <FrameworkListSearchFilter/>
+        {/* <FrameworkListSearchFilter/>> */}
+        <ResponsiveText/>
         </div>
     )
